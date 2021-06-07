dd68e829351d38b6bd25f1c43e40c56ed565113e
prithvsi <prithvsi@in.ibm.com>
2021-06-07 19:21:19 +0530

    fix: formating of config file

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/Prithvipal/react-github-action",
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
-    ]
-}
+  branches: "main",
+  repositoryUrl: "https://github.com/Prithvipal/react-github-action",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
