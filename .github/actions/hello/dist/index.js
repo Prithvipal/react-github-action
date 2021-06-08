be0672e682fc8156601080b0504260d2b95f13f7
prithvsi <prithvsi@in.ibm.com>
2021-06-08 16:46:03 +0530

    more features of core

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -621,12 +621,19 @@ try{
     // throw new Error("Some error message")
     const name = core.getInput('who-to-greet')
 
+    core.debug("Debug Message")
+    core.warning("Warning Message")
+    core.error("Error Message")
+
+    core.setSecret(name)
     console.log(`Helloooo00 ${name}`)
     
     const time = new Date()
     core.setOutput("time", time.toTimeString())
     
+    core.startGroup("Logging github object")
     console.log(JSON.stringify(github, null, '\t'))
+    core.endGroup()
 }catch(error){
     core.setFailed(error.message)
 }
