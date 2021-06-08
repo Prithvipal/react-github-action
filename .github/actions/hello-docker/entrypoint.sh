74d302f2326944d4269d423342781d8c51b94e6f
prithvsi <prithvsi@in.ibm.com>
2021-06-08 19:05:54 +0530

    logs

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh -l
 
+echo "::debug ::Debug Message"
+echo "::warning ::Debug Message"
+echo "::error ::Debug Message"
+
+echo "::add-mask::$1"
 echo "Hellooo $1"
+
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo "some stuff1"
+echo "some stuff2"
+echo "some stuff3"
+echo "::endgroup::"
+
+echo "::set-env name=HOLA::Hey-Hello"
+
