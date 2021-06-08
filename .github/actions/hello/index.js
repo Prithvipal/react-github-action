18575583673965f80699e29567e4132d7378989e
prithvsi <prithvsi@in.ibm.com>
2021-06-08 19:45:09 +0530

    fix: file formats

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,25 +1,25 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try{
-    // throw new Error("Some error message")
-    const name = core.getInput('who-to-greet')
+try {
+  // throw new Error("Some error message")
+  const name = core.getInput("who-to-greet");
 
-    core.debug("Debug Message")
-    core.warning("Warning Message")
-    core.error("Error Message")
+  core.debug("Debug Message");
+  core.warning("Warning Message");
+  core.error("Error Message");
 
-    core.setSecret(name)
-    console.log(`Helloooo00 ${name}`)
-    
-    const time = new Date()
-    core.setOutput("time", time.toTimeString())
-    
-    core.startGroup("Logging github object")
-    console.log(JSON.stringify(github, null, '\t'))
-    core.endGroup()
+  core.setSecret(name);
+  console.log(`Helloooo00 ${name}`);
 
-    core.exportVariable("HELLO", "hhelloo")
-}catch(error){
-    core.setFailed(error.message)
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.startGroup("Logging github object");
+  console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hhelloo");
+} catch (error) {
+  core.setFailed(error.message);
 }
